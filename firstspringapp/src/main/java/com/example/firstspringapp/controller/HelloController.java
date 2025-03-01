fb0a2907b27b41d4b9a52e1f22efe72d4fd9e1de
aYush_agarWaL <ayushofficial4208@gmail.com>
2025-03-01 23:10:23 +0530

    uc-5 commited

--- firstspringapp/src/main/java/com/example/firstspringapp/controller/HelloController.java
+++ firstspringapp/src/main/java/com/example/firstspringapp/controller/HelloController.java
@@ -25,4 +25,10 @@ public class HelloController {
     public String sayHello(@RequestBody User user) {
         return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
     }
+
+
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
+    }
 }
